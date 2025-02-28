ba376eb431d34359d24a1fe3c65b1c06400b84a5
pranav-admin-devops <pranav.kamble.mail@gmail.com>
2025-02-28 21:31:50 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -558,13 +558,13 @@ resource "azurerm_resource_group" "rg" {
   location = "centralindia"
 }
 
-# resource "azurerm_virtual_network" "vnet" {
-#   name                = "pranav-network"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   address_space       = ["10.0.0.0/16"]
-#   dns_servers         = ["10.0.0.4", "10.0.0.5", "8.8.8.8"]
-# }
+resource "azurerm_virtual_network" "vnet" {
+  name                = "pranav-network"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5", "8.8.8.8"]
+}
 
 # resource "azurerm_subnet" "sub" {
 #   name                 = "pranav-subnet"
