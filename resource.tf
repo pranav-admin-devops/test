e834d288cb2e9246168c1f5dcfbf181afdb5ea20
pranav <pranav@blah.com>
2025-02-28 15:17:34 +0530

    chatbot commit

--- resource.tf
+++ resource.tf
@@ -150,3 +150,12 @@ resource "azurerm_linux_virtual_machine" "linux-vm" {
     version   = "latest"
   }
 }
+
+module "avm-res-network-virtualnetwork" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "0.8.1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  address_space = ["40.0.0.0/16"]
+  name = "module-vnet"
+}
